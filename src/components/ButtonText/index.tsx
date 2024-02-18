6cd9151136fb99cb1943e1760517271fcb083fc3
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-17 22:05:15 -0300

    FIX: Parameter correction in styled components

--- src/components/ButtonText/index.tsx
+++ src/components/ButtonText/index.tsx
@@ -2,11 +2,11 @@ import { Container } from "./styles";
 import { FC } from "react";
 import ButtonTextProps from "./ButtonTextProps";
 
-const ButtonText: FC<ButtonTextProps> = ({ children, isActive, ...props }) => {
+const ButtonText: FC<ButtonTextProps> = ({ children, $isActive, ...props }) => {
   return (
     <Container
       type="button"
-      $isActive={isActive}
+      $isActive={$isActive}
       {...props}
     >
       {children}
